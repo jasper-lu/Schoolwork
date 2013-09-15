194cd1f6d81ae50a5e1d391cf127acc2399d6812
Jasper Lu <repsajrules@gmail.com>
2013-09-14 18:06:47 -0700

    init

--- Systems/Lesson1/hello.c
+++ Systems/Lesson1/hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void main() {
-    printf("hello world! \n");
+    int i, n,gtfo;
+    long a = -102, b;
+    int xy[2]0;
+    xy[5] = -2;
+    xy[0] = 12;
+    xy[20] = 3;
+    //this would normally make an out of bounds error, but not in c. Instead, what happens here is that it adds to another piece of memory, with a reference x[20]. However, that piece of memory may have already been allocated to a different variable, which would cause a "hidden" error. C doesn't check for errors here becasue it is built for absolute efficiency. 
 }
